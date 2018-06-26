fa512e09fb406957df7cdaf6262d38e16b961a46
Antoine Benain <antoine.benain@gmail.com>
2018-06-26 22:46:04 +0200

    Send multiple positions and crash the server

--- lib.js
+++ lib.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const {WS_URL, USER_REPORT_PERIOD_MS, NB_CONCURRENT_POS_REPORT} = require('./config');
 const AUTH_URL = `${WS_URL}/auth/local`;
 const RACE_URL = `${WS_URL}/api/v2/races`;
+const CREATE_POSITION_URL = `${WS_URL}/api/v2/positions/races`;
 const TOKEN_PREFIX = 'Bearer ';
 
 function getUsersWithTokens(users){
@@ -52,9 +53,11 @@ function getInitialUsersInRacesStatus(users, races){
 		usersInRacesStatus[user.email] = {
 			email: user.email,
 			id: user._id,
-			races: races.map(race => ({
-				id: race._id,
-				name: race.name,
+			token: user.token,
+			races: races.map(({_id: id, name, trackpoints}) => ({
+				id,
+				name,
+				trackpoints,
 				index: 0,
 			}))
 		};
@@ -62,8 +65,45 @@ function getInitialUsersInRacesStatus(users, races){
 	return usersInRacesStatus;
 }
 
+function sendPosition(user, race, position){
+	const {lat: latitude, lng: longitude, ele: elevation} = position;
+	const headers = getAuthorizationHeaderForUser(user);
+	return axios.post(`${CREATE_POSITION_URL}/${race.id}`, {latitude, longitude, elevation}, {headers});
+
+}
+
+function getRandomNoise(){
+	return Math.floor(Math.random() * 10) / 10000;
+}
+
+function getNextPosition({index, trackpoints}){
+	const trackpoint = trackpoints[index];
+	return {
+		...trackpoint,
+		lat: trackpoint.lat + getRandomNoise(),
+		lng: trackpoint.lng + getRandomNoise(),
+	};
+}
+
+function sendPositionsForUserInRace(user, race){
+	console.log(`sending ${NB_CONCURRENT_POS_REPORT} pos for user ${user.email} in race ${race.name}`);
+	const promises = [];
+	for(let i=0; i < NB_CONCURRENT_POS_REPORT; i++){
+		const position = getNextPosition(race);
+		race.index = (race.index + 1) % race.trackpoints.length;
+		promises.push(sendPosition(user, race, position));
+	}
+
+	return Promise.all(promises);
+}
+
 const getUserTimer = userInRaces => setInterval(() => {
-	console.log(`sending ${NB_CONCURRENT_POS_REPORT} pos for user ${userInRaces.email}`);
+	userInRaces.races.forEach(race => {
+		const {email, id, token} = userInRaces;
+		sendPositionsForUserInRace({email, id, token}, race).catch(error => {
+			console.log(`create position request failed: ${error}`);
+		})
+	});
 }, USER_REPORT_PERIOD_MS);
 
 module.exports = {
