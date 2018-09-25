c64cf19fcb71974ffffef0db67a47e05cb9c2627
Antoine Benain <antoine.benain@gmail.com>
2018-09-25 22:42:34 +0200

    Use more realistic position creation frequencies

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const WS_URL = 'https://test.followracer.com';	//The website url
 const TEST_DURATION_MS = 10 * 60 * 1000;	//How long shall tests run?
 
 //For the positions creation tests
-const USER_REPORT_PERIOD_MS = 6 * 1000;	//How often shall each user/participant report positions?
-const NB_CONCURRENT_POS_REPORT = 25;		//How many positions shall be reported at every iteration?
+const USER_REPORT_PERIOD_MS = 30 * 1000;	//How often shall each user/participant report positions?
+const NB_CONCURRENT_POS_REPORT = 6;		//How many positions shall be reported at every iteration?
 
 //for the viewer test
 const NB_VIEWERS 				= 10;		//How many user are viewing the livetracking page?
